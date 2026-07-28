f1a6d67bd3c595ea74e3f9617e22968a1232b7e0
Scott Nixon <citadelgrad@gmail.com>
2026-07-28 14:02:16 -0700

    fix: reset WezTerm configuration

--- dotfiles/home/dot_config/fish/functions/yts.fish
+++
@@ -1,4 +0,0 @@
-function yts --description "YouTube video summary using fabric-ai"
-    set -l video_link $argv[1]
-    fabric-ai -y "$video_link" --stream --pattern summarize
-end
--- dotfiles/home/dot_config/fish/functions/ytw.fish
+++
@@ -1,4 +0,0 @@
-function ytw --description "Extract wisdom from YouTube video using fabric-ai"
-    set -l video_link $argv[1]
-    fabric-ai -y "$video_link" --stream --pattern extract_wisdom
-end
